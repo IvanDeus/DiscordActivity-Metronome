0d2cf25bfc206eaa278471dfd9c65acad102090b
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2026-06-09 17:25:37 +0300

    Create app_cfg.example.py

---
+++ app_cfg.example.py
@@ -0,0 +1,17 @@
+# app_cfg.example.py - Example configuration for your Flask application
+
+# Discord App Credentials
+DISCORD_CLIENT_ID = 'YOUR_DISCORD_CLIENT_ID'
+DISCORD_CLIENT_SECRET = 'YOUR_DISCORD_CLIENT_SECRET'
+
+# Logging settings
+LOGFPATH = "/var/log/DiscordActivity-Metronome.log"
+
+# DB name
+DATABASE = 'users_db.sqlite'
+
+# Local port for app
+APP_LPORT = 6543
+
+# Application Settings
+DEBUG = False
